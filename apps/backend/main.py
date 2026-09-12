784b1ff94253aad2cb4cf34f043f23ec54c1289f
Ma115tte <73129460+Ma115tte@users.noreply.github.com>
2026-09-12 13:03:03 +0200

    Create main.py
    Minimal script for test purposes (if this file is empty then the container crashes)

---
+++ apps/backend/main.py
@@ -0,0 +1,18 @@
+"""
+Minimal script to test the stack (empty main.py --> crash) and check whether psql is up or not
+"""
+
+
+import os
+
+from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+
+app = FastAPI()
+engine = create_engine(os.environ["DATABASE_URL"])
+
+
+@app.get("/api/health")
+def health():
+    with engine.connect() as conn:
+        return {"db": conn.execute(text("SELECT 1")).scalar()}
